8de352aa9322d848c7668440e05df22f3e676d2e
mwaisberg <mwaisberg@721b37f6-5e27-482b-a502-8568af644790>
2015-12-30 13:58:31 +0000

    poll.

--- code/trunk/Power Failure/app/src/main/java/net/sf/power/monitor/MainActivity.java
+++ code/trunk/Power Failure/app/src/main/java/net/sf/power/monitor/MainActivity.java
@@ -1,13 +1,107 @@
 package net.sf.power.monitor;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.format.DateUtils;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
 
 public class MainActivity extends Activity {
+
+    private static final long POLL_RATE = DateUtils.SECOND_IN_MILLIS;
+
+    private TextView labelView;
+
+    private static IntentFilter batteryFilter;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        labelView = (TextView) findViewById(R.id.label);
+
+        handler = new MainHandler(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(this);
+    }
+
+    private void registerReceiver(Context context) {
+        printStatus(context);
+        pollStatus();
+    }
+
+    private void unregisterReceiver(Context context) {
+        // Sticky intent doesn't need to unregister.
+        if (handler != null) {
+            handler.removeMessages(MainHandler.CHECK_STATUS);
+        }
+    }
+
+    protected Intent getBatteryIntent(Context context) {
+        IntentFilter filter = batteryFilter;
+        if (filter == null) {
+            filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            batteryFilter = filter;
+        }
+        return context.registerReceiver(null, filter);
+    }
+
+    private void checkStatus() {
+        printStatus(this);
+        pollStatus();
+    }
+
+    private void pollStatus() {
+        if (handler != null) {
+            handler.sendEmptyMessageDelayed(MainHandler.CHECK_STATUS, POLL_RATE);
+        }
+    }
+
+    private void printStatus(Context context) {
+        Intent intent = getBatteryIntent(context);
+        Bundle extras = intent.getExtras();
+        int plugged = extras.getInt(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean present = extras.getBoolean(BatteryManager.EXTRA_PRESENT, false);
+        int status = extras.getInt(BatteryManager.EXTRA_STATUS, -1);
+        System.out.println("~!@ status=" + status + " plugged=" + plugged + " present=" + present);
+    }
+
+    private static class MainHandler extends Handler {
+
+        private static final int CHECK_STATUS = 1;
+
+        private final WeakReference<MainActivity> activity;
+
+        public MainHandler(MainActivity activity) {
+            this.activity = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case CHECK_STATUS:
+                    activity.get().checkStatus();
+                    break;
+            }
+        }
     }
 }
--- code/trunk/Power Failure/app/src/main/java/net/sf/power/monitor/PowerConnectionReceiver.java
+++ code/trunk/Power Failure/app/src/main/java/net/sf/power/monitor/PowerConnectionReceiver.java
@@ -3,6 +3,7 @@ package net.sf.power.monitor;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.BatteryManager;
 import android.util.Log;
 
 /**
@@ -16,5 +17,10 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, intent.toString());
+
+        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        System.out.println("~!@ onReceive status=" + status);
+        int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        System.out.println("~!@ onReceive plugged=" + plugged);
     }
 }
