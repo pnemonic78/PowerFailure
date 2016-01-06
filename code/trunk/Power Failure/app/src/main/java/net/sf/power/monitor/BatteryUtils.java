c2d1ef467350c16ae070d100a73c10d688ef8ada
mwaisberg <mwaisberg@721b37f6-5e27-482b-a502-8568af644790>
2016-01-06 12:30:08 +0000

    rights.

--- code/trunk/Power Failure/app/src/main/java/net/sf/power/monitor/BatteryUtils.java
+++ code/trunk/Power Failure/app/src/main/java/net/sf/power/monitor/BatteryUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Source file of the Power Failure Monitor project.
+ * Copyright (c) 2016. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/2.0
+ *
+ * Contributors can be contacted by electronic mail via the project Web pages:
+ *
+ * https://sourceforge.net/projects/power-failure/
+ *
+ * Contributor(s):
+ *   Moshe Waisberg
+ *
+ */
 package net.sf.power.monitor;
 
 import android.content.Context;
