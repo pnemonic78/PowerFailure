7206fc2e0584262f0725592b1cebc028d59d72a1
M. Waisberg <pnemonic@gmail.com>
2020-06-15 13:23:49 +0300

    migrate Fabric Crashlytics to Firebase Crashlytics.

--- Power Failure/app/src/main/kotlin/net/sf/power/monitor/PowerMonitorApplication.kt
+++ Power Failure/app/src/main/kotlin/net/sf/power/monitor/PowerMonitorApplication.kt
@@ -1,16 +1,13 @@
 package net.sf.power.monitor
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
 import com.github.util.LogTree
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class PowerMonitorApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Fabric.with(this, Crashlytics())
         Timber.plant(LogTree(BuildConfig.DEBUG))
     }
 }
