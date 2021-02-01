8ca8dcc6994a03eac5212f436fde4da7c3c5ec18
M. Waisberg <pnemonic@gmail.com>
2021-02-01 09:05:22 +0200

    migrate test to androidx

--- Power Failure/app/src/androidTest/java/net/sf/power/monitor/ApplicationTest.java
+++ Power Failure/app/src/androidTest/java/net/sf/power/monitor/ApplicationTest.java
@@ -1,8 +1,10 @@
 package net.sf.power.monitor;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +22,7 @@ public class ApplicationTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        final Context appContext = InstrumentationRegistry.getTargetContext();
+        final Context appContext = ApplicationProvider.getApplicationContext();
         assertEquals("net.sf.power.monitor.debug", appContext.getPackageName());
     }
 }
