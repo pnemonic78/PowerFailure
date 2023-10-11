67c7289cdb243b1471410bc12ef863329cc03046
M. Waisberg <pnemonic@gmail.com>
2023-10-11 17:08:06 +0300

    vibrator

--- Power Failure/app/src/main/kotlin/net/sf/power/monitor/notify/NotifyVibrate.kt
+++ Power Failure/app/src/main/kotlin/net/sf/power/monitor/notify/NotifyVibrate.kt
@@ -1,17 +1,12 @@
 package net.sf.power.monitor.notify
 
-import android.Manifest
 import android.content.Context
-import android.os.Build
-import android.os.VibrationAttributes
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.os.VibratorManager
 import android.text.format.DateUtils
-import androidx.annotation.RequiresApi
-import androidx.annotation.RequiresPermission
+import com.github.os.VibratorCompat
 
-class NotifyVibrate(private val context: Context) {
+class NotifyVibrate(context: Context) {
+
+    private val vibrator = VibratorCompat(context)
     private var isVibrating: Boolean = false
 
     /**
@@ -20,62 +15,15 @@ class NotifyVibrate(private val context: Context) {
      * @param vibrate `true` to start vibrating - `false` to stop.
      */
     fun vibrate(vibrate: Boolean) {
-        val context: Context = context
-        val vibrator = getVibrator(context)
         if (vibrate) {
-            if (!isVibrating && vibrator.hasVibrator()) {
+            if (!isVibrating) {
                 isVibrating = true
-                vibrate(vibrator)
+                vibrator.vibrate(VIBRATE_PATTERN, VibratorCompat.USAGE_ALARM)
             }
         } else if (isVibrating) {
             isVibrating = false
             vibrator.cancel()
         }
-    }
-
-    private fun getVibrator(context: Context): Vibrator {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val manager =
-                context.getSystemService(VibratorManager::class.java)
-            manager.defaultVibrator
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            context.getSystemService(Vibrator::class.java)
-        } else {
-            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        }
-    }
-
-    @RequiresPermission(Manifest.permission.VIBRATE)
-    private fun vibrate(vibrator: Vibrator) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            vibrate33(vibrator)
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            vibrate26(vibrator)
-        } else {
-            vibrateLegacy(vibrator)
-        }
-    }
-
-    @Suppress("DEPRECATION")
-    @RequiresPermission(Manifest.permission.VIBRATE)
-    private fun vibrateLegacy(vibrator: Vibrator) {
-        vibrator.vibrate(VIBRATE_PATTERN, 0)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    @RequiresPermission(Manifest.permission.VIBRATE)
-    private fun vibrate26(vibrator: Vibrator) {
-        val vibe: VibrationEffect = VibrationEffect.createWaveform(VIBRATE_PATTERN, 0)
-        vibrator.vibrate(vibe)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    @RequiresPermission(Manifest.permission.VIBRATE)
-    private fun vibrate33(vibrator: Vibrator) {
-        val vibe: VibrationEffect = VibrationEffect.createWaveform(VIBRATE_PATTERN, 0)
-        val attributes: VibrationAttributes =
-            VibrationAttributes.createForUsage(VibrationAttributes.USAGE_ALARM)
-        vibrator.vibrate(vibe, attributes)
     }
 
     companion object {
