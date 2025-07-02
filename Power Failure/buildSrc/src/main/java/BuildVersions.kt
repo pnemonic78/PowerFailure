9bbab672ec9807bd3a89c294f6e24950b578f596
Moshe Waisberg <moshe.waisberg@pendo.io>
2025-07-02 20:23:53 +0300

    Migrate to Android 35+

--- Power Failure/buildSrc/src/main/java/BuildVersions.kt
+++ Power Failure/buildSrc/src/main/java/BuildVersions.kt
@@ -1,13 +1,13 @@
 import org.gradle.api.JavaVersion
 
 object BuildVersions {
-    const val agp = "8.4.2"
+    const val agp = "8.10.1"
     const val kotlin = "2.0.0"
     val jvm = JavaVersion.VERSION_11
 
     const val minSdk = 21
-    const val compileSdk = 34
-    const val targetSdk = 34
+    const val compileSdk = 36
+    const val targetSdk = 36
 
     // App dependencies
     const val androidTest = "1.6.1"
